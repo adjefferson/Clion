e59bae8e67afe17df4a123cd0cb8539de88fa7e4
adjefferson <austindjefferson@gmail.com>
2025-07-07 12:23:12 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+std::ifstream ifile;
+
+void calculateEarnings(string name, string id, double payrate);
+
+
+
+
+
+
+int main()
+{
+    //READ EMPLOYEES IDENTIFICATION FROM FILE
+   std::ifstream inputFile("employees.txt");
+
+
+
+
+
+
+
+string EmployeeID [10];
+string EmployeeName[10];
+double EmployeePayRate[10];
+
+inputFile >> EmployeeID[0];
+inputFile >> EmployeeName[0];
+inputFile >> EmployeePayRate[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
